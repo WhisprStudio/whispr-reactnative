c07a0e5856d793e3787701e54bf4c7ee30794653
Lucas Cherloneix <lcherloneix@localhost.localdomain>
2021-06-09 16:26:30 +0200

    ADD- useFonts installation and testing features

--- whispr-app/App.tsx
+++ whispr-app/App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+
 export default function App() {
+  const [loaded] = useFonts({
+    BarlowBold: require('./assets/fonts/Barlow-Bold.ttf'),
+    BarlowItalic: require('./assets/fonts/Barlow-Italic.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: 'BarlowBold', fontSize: 30 }}>Barlow-Bold</Text>
+      <Text style={{ fontFamily: 'BarlowItalic', fontSize: 30 }}>Barlow-Italic</Text>
       <StatusBar style="auto" />
     </View>
   );
