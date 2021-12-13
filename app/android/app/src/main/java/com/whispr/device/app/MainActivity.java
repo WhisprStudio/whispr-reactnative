98154367a9c20e3b6b5347ac73f7f3a13c6cb21b
Antonin Keuck <antonin.keuck@epitech.eu>
2021-12-13 15:45:11 -0800

    [UPDATE] package name, app name and logo

--- app/android/app/src/main/java/com/whispr/device/app/MainActivity.java
+++ app/android/app/src/main/java/com/whispr/device/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.app;
+package com.whispr.device.app;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
--- app/android/app/src/main/java/com/whispr/device/app/MainApplication.java
+++ app/android/app/src/main/java/com/whispr/device/app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.app;
+package com.whispr.device.app;
 
 import android.app.Application;
 import android.content.Context;
