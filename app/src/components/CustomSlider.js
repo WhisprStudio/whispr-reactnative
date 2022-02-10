8adf830b55266fd71ca25390b6acf76bedc93aec
Antonin Keuck <antonin.keuck@epitech.eu>
2022-02-10 10:30:28 -0800

    [FIX] slider setup first value and update onChange handling #2

--- app/src/components/CustomSlider.js
+++ app/src/components/CustomSlider.js
@@ -5,11 +5,20 @@ import {View, Text} from "react-native";
 import {theme} from "@theme";
 
 export default function CustomSlider(props) {
+const [givenValue, setGivenValue] = useState(props.value)
+
+  useEffect(() => {
+    if ( props.newValue ) {
+      setGivenValue(props?.newValue)
+    }
+  }, [props?.newValue]);
+
+
 
   return (
   <View>
             <Text style={styles.text}>{props.title}</Text>
-            <Slider style={{height: 40, margin: 0, padding: 0}} onValueChange={(value) => {props.setValue ? props.setValue(value): console.log("error")}} value={props.value} minimumValue={0} maximumValue={100} minimumTrackTintColor={theme.colors.yellow} thumbTintColor={"#fff"}/>
+            <Slider style={{height: 40, margin: 0, padding: 0}} onValueChange={(value) => {props.setValue ? props.setValue(value): console.log("error")}} value={givenValue} minimumValue={0} maximumValue={100} minimumTrackTintColor={theme.colors.yellow} thumbTintColor={"#fff"}/>
     </View>
   );
 }
